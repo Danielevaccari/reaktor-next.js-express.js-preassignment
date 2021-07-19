374a1b48a17bf05543bdae2ef14cce17e2714a33
Daniele Vaccari <daniele.j.vaccari@gmail.com>
2021-07-19 09:15:11 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+
+  const [strMagic, setStrMagic] = useState('')
+
+  const changeStrMagic = (e) => {
+    setStrMagic(e)
+  }
+
+  const dataFetch = async () => {
+    const res = await fetch('https://newest-react-express.herokuapp.com/')
+    const data = await res.text()
+    changeStrMagic(data)
+  }
+
+  useEffect(() => {
+    dataFetch()
+  }, [])
+
+  return (
+    <>
+      <div>{strMagic}</div>
+    </>
+  )
+}
