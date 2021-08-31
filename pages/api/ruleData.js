44f5bc58542ccb04da4c9f116bd38d2e0bbf52cc
Daniele Vaccari <daniele.j.vaccari@gmail.com>
2021-08-31 09:12:20 +0300

    changed api name

--- pages/api/ruleData.js
+++ pages/api/ruleData.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+//Fetches .txt data to https://reaktor-next-js-express-js-preassignment.vercel.app/api/ruledata
 export default async function handler(req, res) {
   const response = await fetch('https://media.wizards.com/2021/downloads/MagicCompRules%2020210419.txt')
   res.send(await response.text());
